4b5cc585df21b852ae7fe9c4fc88d684b47ebdb3
William Fridh <William Fridh>
2024-03-18 19:47:07 +0100

    Prog..

--- next.config.js
+++ next.config.js
@@ -1,26 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-
-
 module.exports = {
 	async redirects() {
-	  return [
-		{
-		  source: '/',
-		  destination: process.env.WORDPRESS_DEFAULT_SLUG,
-		  permanent: true,
-		},
-	  ];
+		return [
+			{
+				source: '/',
+				destination: process.env.WORDPRESS_DEFAULT_SLUG,
+				permanent: true,
+			},
+		];
 	},
-
 	images: {
 		remotePatterns: [
+			// Used for allowing loading assets from remote sources.
 			{
 				protocol: 'https',
 				hostname: 'wp.williamfridh.com'
 			}
 		]
 	}
-	
 };
 
