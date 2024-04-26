b20dd015331ed9893bfa67490a6fc7db31fd1cdd
Fridh <william.fridh@gmail.com>
2024-04-26 15:42:56 +0200

    Added cms theme

---
+++ cms/RedirectionTheme/index.php
@@ -0,0 +1,7 @@
+<main id="primary" class="site-main">
+    <section class="error-404 not-found">
+        <header class="page-header">
+            <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'custom-404' ); ?></h1>
+        </header><!-- .page-header -->
+    </section><!-- .error-404 -->
+</main><!-- #main -->
