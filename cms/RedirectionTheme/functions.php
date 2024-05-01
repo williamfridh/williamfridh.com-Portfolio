eb7374252eb5a65218157e05ff2dcbcdfaaff8d2
William Fridh <william.fridh@gmail.com>
2024-05-01 18:30:00 +0200

    Added support for feature images on posts
    This disappeared with the new theme.

--- cms/RedirectionTheme/functions.php
+++ cms/RedirectionTheme/functions.php
@@ -1,16 +1,23 @@
 <?php
 /**
  * Redirection Theme functions and definitions.
- * This is only required for makeing the theme compatible with the WordPress REST API.
+ * This is only required for making the theme compatible with the WordPress REST API.
  * 
  * SRC: https://github.com/wp-graphql/wp-graphql/issues/2103
  */
+
+add_action('after_setup_theme', 'theme_setup');
+function theme_setup() {
+	// Add support for post thumbnails
+	add_theme_support('post-thumbnails');
+}
+
 add_filter('graphql_data_is_private', function ($is_private, $model_name, $data, $visibility, $owner, $current_user) {
 
 	if ('MenuObject' === $model_name || 'MenuItemObject' === $model_name) {
 		return false;
 	}
 
-	return $is_private;
+  	return $is_private;
 }, 10, 6);
 
